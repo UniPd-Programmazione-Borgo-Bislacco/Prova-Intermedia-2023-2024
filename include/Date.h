aef48ba0ae9ac1cb275d1ee5996140d3cc533cc0
GuoLozZz <davideguolo.1909@gmail.com>
2023-11-20 20:37:38 +0100

    Creato header Date

--- include/Date.h
+++ include/Date.h
@@ -1,6 +1,30 @@
 class Date{
-public:
+
+public:    
+
+    Date (int y, Month m, int d);
+    
+    int day() const;
+    Month month() const;
+    int year() const; 
+
+    bool operator==(const Date& a);
+    bool operator!=(const Date& a);
+    bool operator>(const Date& a);
+    bool operator<(const Date& a);
+    bool operator>=(const Date& a);
+    bool operator<=(const Date& a);
 
 private:
+    int y, d;
+    Month m;
+    bool isValid();
+};
 
-}
+
+
+//Sono diventato morte...il distruttore di header
+
+enum class Month{
+    jan=1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec
+};
