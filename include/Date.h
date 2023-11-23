b961f2a160d06895a6de45cff81d07b05217cb4b
GuoLozZz <davideguolo.1909@gmail.com>
2023-11-23 19:38:30 +0100

    Aggiunti costruttori copy e move(da finire)

--- include/Date.h
+++ include/Date.h
@@ -9,7 +9,10 @@ public:
     enum Month{
         jan=1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec
     };
+    //Costruttori
     Date (int y, Month m, int d);
+    Date(Date&& old);       //Move constructor
+	Date(Date& old);        //Costruttore per copia
     //CHECK se reference o meno
     Date& operator=(const Date& a);
 
@@ -20,8 +23,8 @@ public:
 private:
     int y_, d_;
     Month m_;
-    bool isLeap();
-    bool isValid();
+    bool isLeap();  //Controllo dell'anno bisestile
+    bool isValid(); //Controllo validità della data
 };
 
 bool operator==(const Date& a, const Date& b);
