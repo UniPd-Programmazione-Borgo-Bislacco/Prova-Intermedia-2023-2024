12a143593b4fd0b2c15251842cc883b007067986
Heldingar <84403208+Heldingar@users.noreply.github.com>
2023-11-24 23:13:23 +0100

    Messo in pari Book - push forzato

--- src/Book.cpp
+++ src/Book.cpp
@@ -4,34 +4,16 @@
 // #include <string>
 #include <stdexcept>
 
-/*Book::Book(bool valid)
-  :name_{""},surname_{""},title_{""}, isbn_{""},date_{valid},is_available_{false}
-{
+//TODO controllo DATA
+Book::Book(std::string name, std::string surname, std::string title, std::string isbn, Date copyright_date,  bool is_available) :
+    name_{name.size()<1? throw std::invalid_argument("Inserire un nome valido."):name},
+    surname_{surname.size()<1? throw std::invalid_argument("Inserire un cognome valido."):surname},
+    title_{title.size()<1? throw std::invalid_argument("Inserire un title valido."):title},
+    isbn_{isbn.size() != 13?throw std::invalid_argument("Il codice ISBN deve essere di 13 caratteri."):isbn},
+    copyright_date_{copyright_date}, 
+    is_available_{is_available}
 
-}*/
-Book::Book(std::string name, std::string surname, std::string title, std::string isbn,  bool is_available)
-     :name_{name},surname_{surname},title_{title}, isbn_{isbn},copyright_date_{Date()}, is_available_{is_available}
-{
-        if (isbn.size() != 13) {
-            throw std::invalid_argument("Il codice ISBN deve essere di 13 caratteri.");
-        }
-}
-Book::Book(std::string name, std::string surname, std::string title, std::string isbn, Date copyright_date,  bool is_available)
-     :name_{name},surname_{surname},title_{title}, isbn_{isbn},copyright_date_{copyright_date}, is_available_{is_available}
-
-{
-if (isbn.size() != 13) {
-    throw std::invalid_argument("Il codice ISBN deve essere di 13 caratteri.");
-  }
-}
-
-// Book::Book(std::string name, std::string surname, std::string title, std::string isbn,  bool is_available)
-//   :name_{name},surname_{surname},title_{title}, isbn_{isbn},is_available_{is_available}
-// {
-//   if (isbn.size() != 13) {
-//     throw std::invalid_argument("Il codice ISBN deve essere di 13 caratteri.");
-//   }
-// }
+{}
 
 Book::Book(Book&& old)
   :name_{old.name_},surname_{old.surname_},title_{old.title_},isbn_{old.isbn_},copyright_date_{old.copyright_date_},is_available_{old.is_available_}
@@ -43,8 +25,9 @@ Book::Book(Book&& old)
   old.setAvailable(false);
 }
 Book::Book(const Book& old)
-  :name_{old.name_},surname_{old.surname_},title_{old.title_},isbn_{old.isbn_},copyright_date_{old.copyright_date_},is_available_{old.is_available_}
+  :name_{old.name_},surname_{old.surname_},title_{old.title_},isbn_{old.isbn_},copyright_date_{old.copyright_date_},is_available_{old.is_available_} 
 {
+
 }
 
 void Book::setIsbn(std::string isbn){
@@ -54,9 +37,9 @@ void Book::setIsbn(std::string isbn){
   isbn_ = isbn;
 }
 
+
 void Book::setTitle(std::string title){title_ = title;}
 void Book::setName(std::string name){name_ = name;}
-
 void Book::setSurname(std::string surname){surname_ = surname;}
 
 // void Book::setDate(Date copyright_date){copyright_date_ = copyright_date;}
@@ -72,7 +55,7 @@ void Book::back(){
     if(is_available_) throw std::logic_error("Il libro era già disponibile. Non può esser stato restituito.");
     setAvailable(true);
 }
-
+//TODO - controlla move copy =
 Book& Book::operator=(const Book& old){
   if(old==(*this))
     return *this;
@@ -103,7 +86,6 @@ Book& Book::operator=(Book&& old){
   return *this;
 }
 
-
 bool operator==(const Book& a,const Book& b){
     return a.isbn()==b.isbn();
 }
@@ -112,11 +94,12 @@ bool operator!=(const Book& a,const Book& b){
     return a.isbn()!=b.isbn();
 }
 std::ostream& operator<<(std::ostream& os, const Book& b){
-    return os <<"Titolo: "<<b.title()<<std::endl
-                <<"Autore: "<<b.name()<<" "<<b.surname()<<std::endl
-                <<"ISBN: " <<b.isbn()<<std::endl;
 
- }
-
-
-
+    os  <<"Titolo: "<<b.title()<<std::endl
+        <<"Autore: "<<b.name()<<" "<<b.surname()<<std::endl
+        <<"ISBN: " <<b.isbn()<<'\n'
+        <<"Data: "<<b.date()<<std::endl;
+    return os;
+  
+  
+}
