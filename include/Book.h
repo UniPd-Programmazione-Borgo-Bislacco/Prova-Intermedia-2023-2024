74707f7d16f86323e3901fe617d86bb4ae65e399
Heldingar <84403208+Heldingar@users.noreply.github.com>
2023-11-24 23:13:50 +0100

    Messo in pari Book - push forzato

--- include/Book.h
+++ include/Book.h
@@ -11,8 +11,8 @@ class Book{
 public:
 
 	//Costruttori xForza
-	Book(std::string name, std::string surname, std::string title, std::string isbn, Date copyright_date ,bool is_available=kDefaultAvailable);
-	Book(std::string name, std::string surname, std::string title, std::string isbn, bool is_available=kDefaultAvailable);
+	Book(std::string name, std::string surname, std::string title, std::string isbn, Date copyright_date=Date() ,bool is_available=kDefaultAvailable);
+	// Book(std::string name, std::string surname, std::string title, std::string isbn, bool is_available=kDefaultAvailable);
 	Book();
 	// Book(std::string name, std::string surname, std::string title, std::string isbn, bool is_available=kDefaultAvailable);
 	Book(Book&& old);       //Move constructor
@@ -26,10 +26,11 @@ public:
 	std::string surname(void) const {return surname_;}
 	std::string title(void) const {return title_;}
 	std::string isbn(void)  const {return isbn_;}
-	//TODO vedere costruttore di copia
-	Date date(void)  {return copyright_date_;}
-	bool isAvailable(void) const {return is_available_;}
+	Date date(void)  const {return copyright_date_;}
 
+	bool isAvailable(void) const {return is_available_;}
+	//TODO: togliere flag
+	bool isDateProvided(void)const{return is_date_provided_;}
 	void setName(std::string name);
 	void setSurname(std::string surname);
 	void setTitle(std::string title);
@@ -37,7 +38,7 @@ public:
 	// void setDate(Date copyright_date);
 	void setAvailable(bool is_available);
 
-	//Per gestire il prestito o il ritorno del libro
+	//Per gestire il prestito o il ritorno del libro. Cambia la flag is_available_ con gli appositi controlli.
 	void rent();
 	void back();
 
@@ -50,11 +51,12 @@ private:
 	std::string title_;
 	std::string isbn_;
 	Date copyright_date_;
-
+	//Flag usata per controllare se è stata passata una data
+	bool is_date_provided_;
+	//Flag per controllare se il libro è disponibile(true)  o in prestito (false)
 	bool is_available_;
 	//Preferisco usare un valore di default anche se è solo un booleano in caso si voglia cambiare se un libro è disponibile di default o meno
-  static constexpr int kDefaultAvailable = true;
-   	// Date kDefaultDate = {2020,Date::Month::mar,1};
+  	static constexpr int kDefaultAvailable = true;
 
 };
 	bool operator==(const Book& a,const Book& b);
