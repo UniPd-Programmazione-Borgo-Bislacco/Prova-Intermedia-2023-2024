e12716f543076f68f7ecebe094dcd6a1bcaac1ab
Heldin <dany311200@gmail.com>
2023-11-25 12:07:31 +0100

    Aggiustato Book e Date
    Book: setDate e costruttori
    Date: migliorati operatori e lo stile di codice

--- include/Date.h
+++ include/Date.h
@@ -9,15 +9,14 @@ public:
     enum Month{
         jan=1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec
     };
-    //Costruttori
-
-    Date ();
+//Costruttori
+   
+    Date ();                            //Costruttore di defualt: mettendola a quella odierna
     Date (int y, Month m, int d);
-    Date(Date&& old);       //Move constructor
-    Date(const Date& old);        //Costruttore per copia
-    //CHECK se reference o meno
-    // Date& operator=(const Date& a);
-    void setDate(int y, Month m, int d);
+    Date(Date&& old);                   //Move constructor
+    Date(const Date& old);              //Costruttore per copia
+
+    void setDate(int y, Month m, int d); //Funzione per cambiare la data
 
     Date& operator=(const Date& a);
     Date& operator=(Date&& a);
@@ -29,10 +28,11 @@ public:
 private:
     int y_, d_;
     Month m_;
-    bool isLeap();  //Controllo dell'anno bisestile
+    bool isLeap();                       //Controllo dell'anno bisestile
     bool isValid(int y, Month m, int d); //Controllo validità della data
 };
 
+//Overload operatori. Helper perché non alterano i valori interni dell'oggetto. Commutatività per operatori binari
 bool operator==(const Date& a, const Date& b);
 bool operator!=(const Date& a, const Date& b);
 bool operator>(const Date& a, const Date& b);
@@ -41,5 +41,4 @@ bool operator>=(const Date& a, const Date& b);
 bool operator<=(const Date& a, const Date& b);
 std::ostream& operator<<(std::ostream& os, Date a);
 
-//Sono diventato morte...il distruttore di header
 #endif
